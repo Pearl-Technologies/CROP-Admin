bcdeffb50586771493fbfb81fcdd7bc096b8a9df
pradeep2165 <pradeepswain2165@gmail.com>
2023-06-06 12:11:30 +0530

    holiday added

---
+++ src/pages/operationsManagement/holidays.js
@@ -0,0 +1,367 @@
+// ** React Imports
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Typography from '@mui/material/Typography'
+import TableContainer from '@mui/material/TableContainer'
+
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import TextField from '@mui/material/TextField'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
+import Divider from '@mui/material/Divider'
+import axios from 'axios'
+import LinearProgress from '@mui/material/LinearProgress'
+import Alert from '@mui/material/Alert'
+import Snackbar from '@mui/material/Snackbar'
+import { styled } from '@mui/material/styles'
+import Table from '@mui/material/Table'
+import TableHead from '@mui/material/TableHead'
+import TableCell from '@mui/material/TableCell'
+import TableBody from '@mui/material/TableBody'
+import ArrowUpDropCircleOutline from 'mdi-material-ui/ArrowUpDropCircleOutline'
+import EditIcon from '@mui/icons-material/Edit'
+import TableRow from '@mui/material/TableRow'
+import Avatar from '@mui/material/Avatar'
+import DeleteForeverSharpIcon from '@mui/icons-material/DeleteForeverSharp'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import moment from 'moment'
+// ** Demo Components Imports
+
+// ** Third Party Styles Imports
+import 'react-datepicker/dist/react-datepicker.css'
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+import Slide from '@mui/material/Slide'
+
+const ImgStyled = styled('img')(({ theme }) => ({
+  width: 120,
+  height: 120,
+  marginRight: theme.spacing(6.25),
+  borderRadius: theme.shape.borderRadius
+}))
+const ButtonStyled = styled(Button)(({ theme }) => ({
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    textAlign: 'center'
+  }
+}))
+
+const Holiday = () => {
+  const [updateStatus, setUpdateStatus] = useState(false)
+  const [imageFile, setImageFile] = useState(null)
+  const [profileImg, setProfileImg] = useState(null)
+  const [data, setData] = useState({
+    holidayDate: '',
+    state: '',
+    holidayName: '',
+    _id: ''
+  })
+  const [holidayList, setHolidayList] = useState([])
+  const [message, setMessage] = useState('')
+  const [update, setUpdate] = useState(false)
+  const [categoryId, setCategoryId] = useState('')
+  const router = useRouter()
+  const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction='up' ref={ref} {...props} />
+  })
+
+  function AlertDialogSlide({ id, categoryName }) {
+    const [open, setOpen] = React.useState(false)
+
+    const handleClickOpen = () => {
+      setOpen(true)
+    }
+
+    const handleClose = () => {
+      setOpen(false)
+    }
+    const handleDelete = async id => {
+      handleClose()
+      // setUpdateStatus(true)
+      const formData = {
+        _id: id
+      }
+      await axios
+        .post(`${process.env.HOST}/api/admin/deleteHoliday`, formData, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}`,
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(function (response) {
+          toast.success(response.data.msg, {
+            position: toast.POSITION.TOP_CENTER,
+            progressClassName: 'Toastify__progress-bar--animated'
+          })
+          setMessage(response.data.msg)
+          // setUpdateStatus(false)
+        })
+        .catch(function (error) {
+          console.log(error)
+          setUpdateStatus(false)
+          let msg = error?.response?.data?.msg
+          setMessage(msg)
+          toast.error(msg, {
+            position: toast.POSITION.TOP_CENTER,
+            progressClassName: 'Toastify__progress-bar--animated'
+          })
+        })
+    }
+    return (
+      <div>
+        <Button onClick={handleClickOpen}>
+          <DeleteForeverSharpIcon />
+        </Button>
+        <Dialog
+          open={open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={handleClose}
+          aria-describedby='alert-dialog-slide-description'
+        >
+          <DialogTitle>{'Delete Holiday'}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id='alert-dialog-slide-description'>Are you really want to delete?</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Disagree</Button>
+            <Button onClick={() => handleDelete(id, categoryName)}>Delete</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    )
+  }
+  const changeHoliday = prop => event => {
+    setData({ ...data, [prop]: event.target.value })
+  }
+  const getAllHolidays = () => {
+    axios
+      .get(`${process.env.HOST}/api/admin/getAllHolidays`, {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`,
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+      .then(function (response) {
+        setHolidayList(response.data.holidays)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error)
+      })
+  }
+  const handleSubmit = async e => {
+    e.preventDefault()
+    setUpdateStatus(true)
+    const formData = {
+      holidayName: data.holidayName,
+      holidayDate: data.holidayDate,
+      state: data.state
+    }
+    await axios
+      .post(`${process.env.HOST}/api/admin/addHoiday`, formData, {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`,
+          'Content-Type': 'Application/json'
+        }
+      })
+      .then(function (response) {
+        toast.success(response.data.msg, {
+          position: toast.POSITION.TOP_CENTER,
+          progressClassName: 'Toastify__progress-bar--animated'
+        })
+        setMessage(response.data.msg)
+        setUpdateStatus(false)
+      })
+      .catch(function (error) {
+        console.log(error)
+        setUpdateStatus(false)
+        let msg = error?.response?.data?.msg
+        setMessage(msg)
+        toast.error(msg, {
+          position: toast.POSITION.TOP_CENTER,
+          progressClassName: 'Toastify__progress-bar--animated'
+        })
+      })
+  }
+  const handleEdit = async id => {
+    let newData = holidayList.filter(x => x._id === id)
+    setData(newData[0])
+    setUpdate(true)
+  }
+  const handleUpdate = async e => {
+    e.preventDefault()
+    setUpdateStatus(true)
+    const formData = {
+      holidayName: data.holidayName,
+      holidayDate: data.holidayDate,
+      state: data.state,
+      _id: data._id
+    }
+    await axios
+      .post(`${process.env.HOST}/api/admin/updateHoliday`, formData, {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`,
+          'Content-Type': 'Application/json'
+        }
+      })
+      .then(function (response) {
+        toast.success(response.data.msg, {
+          position: toast.POSITION.TOP_CENTER,
+          progressClassName: 'Toastify__progress-bar--animated'
+        })
+        setMessage(response.data.msg)
+        setUpdateStatus(false)
+        setUpdate(false)
+      })
+      .catch(function (error) {
+        console.log(error)
+        setUpdateStatus(false)
+        setUpdate(false)
+        let msg = error?.response?.data?.msg
+        setMessage(msg)
+        toast.error(msg, {
+          position: toast.POSITION.TOP_CENTER,
+          progressClassName: 'Toastify__progress-bar--animated'
+        })
+      })
+  }
+
+  const handleCancle = () => {
+    setUpdate(false)
+    setData({
+      holidayName: '',
+      holidayDate: '',
+      state: ''
+    })
+  }
+  useEffect(() => {
+    getAllHolidays()
+    // addASector()
+  }, [message])
+  return (
+    <DatePickerWrapper>
+      {updateStatus && <LinearProgress />}
+      <ToastContainer />
+      <span onClick={() => router.back()}>
+        <ArrowBackIcon  style={{ cursor: 'pointer', margin:"2px"}}/>
+      </span>
+      <Grid container spacing={6}>
+        <Grid item xs={12} md={8}>
+          <Card>
+            <TableContainer sx={{ height: 355 }}>
+              <Table sx={{ minWidth: 300 }} aria-label='table in dashboard' stickyHeader>
+                <TableHead>
+                  {/* <button onClick={() => setShow(x => !x)}>
+                    <ArrowUpDropCircleOutline />
+                  </button> */}
+                  <TableRow>
+                    <TableCell>State</TableCell>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Edit</TableCell>
+                    <TableCell>Delete</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {holidayList.map(row => (
+                    <TableRow hover key={row._id} sx={{ '&:last-of-type td, &:last-of-type th': { border: 0 } }}>
+                      <TableCell>{row.state}</TableCell>
+                      <TableCell>{row.holidayDate}</TableCell>
+                      <TableCell>{row.holidayName}</TableCell>
+                      <TableCell onClick={() => handleEdit(row._id)} sx={{ cursor: 'pointer' }}>
+                        <EditIcon />
+                      </TableCell>
+                      <TableCell>
+                        <AlertDialogSlide id={row._id} />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Card>
+            <CardHeader
+              title={`${update ? 'Update Holiday' : 'New Holiday'}`}
+              titleTypographyProps={{ variant: 'h6' }}
+            />
+            <CardContent>
+              <form onSubmit={e => e.preventDefault()}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} spacing={2}>
+                    <TextField
+                      label={'Name'}
+                      style={{ marginBottom: '8px' }}
+                      value={data.holidayName}
+                      onChange={changeHoliday('holidayName')}
+                    />
+                  </Grid>
+                  <Grid item xs={12} spacing={2}>
+                    <TextField
+                      label={'Date'}
+                      style={{ marginBottom: '8px' }}
+                      value={moment(data.holidayDate).format('YYYY-MM-DD')}
+                      onChange={changeHoliday('holidayDate')}
+                      type='date'
+                    />
+                  </Grid>
+                  <Grid item xs={12} spacing={2}>
+                    <TextField
+                      label={'State'}
+                      style={{ marginBottom: '8px' }}
+                      value={data.state}
+                      onChange={changeHoliday('state')}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Box
+                      sx={{
+                        gap: 5,
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        alignItems: 'center',
+                        justifyContent: 'space-between'
+                      }}
+                    >
+                      {!update && (
+                        <Button type='submit' variant='contained' size='large' onClick={handleSubmit}>
+                          Add
+                        </Button>
+                      )}
+                      {update && (
+                        <>
+                          <Button type='submit' variant='contained' size='large' onClick={handleUpdate}>
+                            Update
+                          </Button>
+                          <Button type='submit' variant='contained' size='large' onClick={handleCancle}>
+                            Cancle
+                          </Button>
+                        </>
+                      )}
+                    </Box>
+                  </Grid>
+                </Grid>
+              </form>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </DatePickerWrapper>
+  )
+}
+
+export default Holiday
--- src/pages/operationsManagement/index.js
+++ src/pages/operationsManagement/index.js
@@ -53,8 +53,7 @@ const OperationsManagement = () => {
         >
           <svg
             xmlns='http://www.w3.org/2000/svg'
-            style={{ marginTop: '20px' }}
-            class='icon icon-tabler icon-tabler-brand-edge'
+            class='icon icon-tabler icon-tabler-propeller'
             width='44'
             height='44'
             viewBox='0 0 24 24'
@@ -63,13 +62,15 @@ const OperationsManagement = () => {
             fill='none'
             stroke-linecap='round'
             stroke-linejoin='round'
+            style={{ marginTop: '20px' }}
           >
             <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-            <path d='M20.978 11.372a9 9 0 1 0 -1.593 5.773' />
-            <path d='M20.978 11.372c.21 2.993 -5.034 2.413 -6.913 1.486c1.392 -1.6 .402 -4.038 -2.274 -3.851c-1.745 .122 -2.927 1.157 -2.784 3.202c.28 3.99 4.444 6.205 10.36 4.79' />
-            <path d='M3.022 12.628c-.283 -4.043 8.717 -7.228 11.248 -2.688' />
-            <path d='M12.628 20.978c-2.993 .21 -5.162 -4.725 -3.567 -9.748' />
+            <path d='M12 13m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0' />
+            <path d='M14.167 10.5c.722 -1.538 1.156 -3.043 1.303 -4.514c.22 -1.63 -.762 -2.986 -3.47 -2.986s-3.69 1.357 -3.47 2.986c.147 1.471 .581 2.976 1.303 4.514' />
+            <path d='M13.169 16.751c.97 1.395 2.057 2.523 3.257 3.386c1.3 1 2.967 .833 4.321 -1.512c1.354 -2.345 .67 -3.874 -.85 -4.498c-1.348 -.608 -2.868 -.985 -4.562 -1.128' />
+            <path d='M8.664 13c-1.693 .143 -3.213 .52 -4.56 1.128c-1.522 .623 -2.206 2.153 -.852 4.498s3.02 2.517 4.321 1.512c1.2 -.863 2.287 -1.991 3.258 -3.386' />
           </svg>
+
           <p style={{ color: '#0053df', height: '50px', cursor: 'pointer', fontWeight: 'bold', fontSize: '15px' }}>
             PROP Management
           </p>
@@ -85,24 +86,21 @@ const OperationsManagement = () => {
             height: '120px'
           }}
         >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            style={{ marginTop: '20px' }}
-            class='icon icon-tabler icon-tabler-brand-edge'
-            width='44'
-            height='44'
-            viewBox='0 0 24 24'
-            stroke-width='1.5'
-            stroke='currentColor'
-            fill='none'
-            stroke-linecap='round'
-            stroke-linejoin='round'
-          >
-            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-            <path d='M20.978 11.372a9 9 0 1 0 -1.593 5.773' />
-            <path d='M20.978 11.372c.21 2.993 -5.034 2.413 -6.913 1.486c1.392 -1.6 .402 -4.038 -2.274 -3.851c-1.745 .122 -2.927 1.157 -2.784 3.202c.28 3.99 4.444 6.205 10.36 4.79' />
-            <path d='M3.022 12.628c-.283 -4.043 8.717 -7.228 11.248 -2.688' />
-            <path d='M12.628 20.978c-2.993 .21 -5.162 -4.725 -3.567 -9.748' />
+          <svg xmlns="http://www.w3.org/2000/svg" 
+          class="icon icon-tabler icon-tabler-tags" 
+          style={{ marginTop: '20px' }}
+          width="44"
+          height="44" 
+          viewBox="0 0 24 24" 
+          stroke-width="1.5" 
+          stroke="#2c3e50" 
+          fill="none" 
+          stroke-linecap="round" 
+          stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M7.859 6h-2.834a2.025 2.025 0 0 0 -2.025 2.025v2.834c0 .537 .213 1.052 .593 1.432l6.116 6.116a2.025 2.025 0 0 0 2.864 0l2.834 -2.834a2.025 2.025 0 0 0 0 -2.864l-6.117 -6.116a2.025 2.025 0 0 0 -1.431 -.593z" />
+          <path d="M17.573 18.407l2.834 -2.834a2.025 2.025 0 0 0 0 -2.864l-7.117 -7.116" />
+          <path d="M6 9h-.01" />
           </svg>
           <p style={{ color: '#0053df', height: '50px', cursor: 'pointer', fontWeight: 'bold', fontSize: '15px' }}>
             Loyalty
@@ -203,7 +201,7 @@ const OperationsManagement = () => {
             <path d='M12.628 20.978c-2.993 .21 -5.162 -4.725 -3.567 -9.748' />
           </svg>
           <p style={{ color: '#0053df', height: '50px', cursor: 'pointer', fontWeight: 'bold', fontSize: '15px' }}>
-          Interests Management
+            Interests Management
           </p>
         </Card>
       </Grid>
@@ -256,7 +254,7 @@ const OperationsManagement = () => {
           <svg
             xmlns='http://www.w3.org/2000/svg'
             style={{ marginTop: '20px' }}
-            class='icon icon-tabler icon-tabler-brand-slack'
+            class='icon icon-tabler icon-tabler-brand-cashapp'
             width='44'
             height='44'
             viewBox='0 0 24 24'
@@ -267,13 +265,44 @@ const OperationsManagement = () => {
             stroke-linejoin='round'
           >
             <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-            <path d='M12 12v-6a2 2 0 0 1 4 0v6m0 -2a2 2 0 1 1 2 2h-6' />
-            <path d='M12 12h6a2 2 0 0 1 0 4h-6m2 0a2 2 0 1 1 -2 2v-6' />
-            <path d='M12 12v6a2 2 0 0 1 -4 0v-6m0 2a2 2 0 1 1 -2 -2h6' />
-            <path d='M12 12h-6a2 2 0 0 1 0 -4h6m-2 0a2 2 0 1 1 2 -2v6' />
+            <path d='M17.1 8.648a.568 .568 0 0 1 -.761 .011a5.682 5.682 0 0 0 -3.659 -1.34c-1.102 0 -2.205 .363 -2.205 1.374c0 1.023 1.182 1.364 2.546 1.875c2.386 .796 4.363 1.796 4.363 4.137c0 2.545 -1.977 4.295 -5.204 4.488l-.295 1.364a.557 .557 0 0 1 -.546 .443h-2.034l-.102 -.011a.568 .568 0 0 1 -.432 -.67l.318 -1.444a7.432 7.432 0 0 1 -3.273 -1.784v-.011a.545 .545 0 0 1 0 -.773l1.137 -1.102c.214 -.2 .547 -.2 .761 0a5.495 5.495 0 0 0 3.852 1.5c1.478 0 2.466 -.625 2.466 -1.614c0 -.989 -1 -1.25 -2.886 -1.954c-2 -.716 -3.898 -1.728 -3.898 -4.091c0 -2.75 2.284 -4.091 4.989 -4.216l.284 -1.398a.545 .545 0 0 1 .545 -.432h2.023l.114 .012a.544 .544 0 0 1 .42 .647l-.307 1.557a8.528 8.528 0 0 1 2.818 1.58l.023 .022c.216 .228 .216 .569 0 .773l-1.057 1.057z' />
           </svg>
           <p style={{ color: '#0053df', height: '50px', cursor: 'pointer', fontWeight: 'bold', fontSize: '15px' }}>
             Payment Process
+          </p>
+        </Card>
+        <Card
+          onClick={() => router.push('/operationsManagement/holidays')}
+          style={{
+            alignItems: 'center',
+            textAlign: 'center',
+            cursor: 'pointer',
+            marginTop: '10px',
+            backgroundColor: '#58D1FC',
+            height: '120px'
+          }}
+        >
+ 
+          <svg xmlns="http://www.w3.org/2000/svg" 
+          style={{ marginTop: '20px' }}
+          class="icon icon-tabler icon-tabler-calendar-event" 
+          width="44" 
+          height="44" 
+          viewBox="0 0 24 24" 
+          stroke-width="1.5" 
+          stroke="#2c3e50" 
+          fill="none" 
+          stroke-linecap="round" 
+          stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+          <path d="M16 3l0 4" />
+          <path d="M8 3l0 4" />
+          <path d="M4 11l16 0" />
+          <path d="M8 15h2v2h-2z" />
+          </svg>
+          <p style={{ color: '#0053df', height: '50px', cursor: 'pointer', fontWeight: 'bold', fontSize: '15px' }}>
+            Holidays
           </p>
         </Card>
       </Grid>
